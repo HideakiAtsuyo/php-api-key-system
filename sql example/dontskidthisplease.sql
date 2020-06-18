d771985d95e5a20c3c7b9c3af16be991d46a16f3
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-06-18 21:32:02 +0200

    Bad start :(

---
+++ sql example/dontskidthisplease.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 18 juin 2020 à 21:30
+-- Version du serveur :  10.1.44-MariaDB-0+deb9u1
+-- Version de PHP : 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `phpkeysystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dontskidthisplease`
+--
+
+CREATE TABLE `dontskidthisplease` (
+  `id` int(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `keybyip` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `dontskidthisplease`
+--
+ALTER TABLE `dontskidthisplease`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
